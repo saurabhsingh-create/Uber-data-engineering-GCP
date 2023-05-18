26c180e359fc6b0744e21672633646a6c4bd4cbb
Saurabh Singh <60736584+saurabhsingh-create@users.noreply.github.com>
2023-05-18 21:54:26 +0530

    Mage files
    python files used in building batch data pipeline

---
+++ data_loader.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+import io 
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    url = 'https://storage.googleapis.com/uber-data-analysis12/uber_data.csv'
+    response = requests.get(url)
+    return pd.read_csv(io.StringIO(response.text), sep=',')
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
